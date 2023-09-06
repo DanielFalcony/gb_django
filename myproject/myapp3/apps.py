f6a757746b3874ec3ac3b4817cbacb20211023b5
Daniil <danielfalcony@gmail.com>
2023-09-06 23:15:32 +0300

    l3

--- myproject/myapp3/apps.py
+++ myproject/myapp3/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Myapp2Config(AppConfig):
+class Myapp3Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seminar_app'
+    name = 'myapp3'
