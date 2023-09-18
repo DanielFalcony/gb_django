6f4533dea9c639cc8a3a827895f83b1f467f8388
Daniil <danielfalcony@gmail.com>
2023-09-18 23:53:08 +0300

    Changes

--- GB_Django/hw2_app/management/commands/hw_add_client.py
+++ GB_Django/hw2_app/management/commands/hw_add_client.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+
+from hw2_app.management.commands.fake_date import generate_random_date
 from hw2_app.models import Client
 
 
@@ -11,11 +13,10 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         count = kwargs.get('count')
         for i in range(1, count + 1):
-            client = Client(name=f'Jhon{i}',
+            client = Client(name=f'John{i}',
                             email=f'a{i}b@mail.ru',
                             phone_number=f'{i * 3945512}',
-                            address=f'some adress house.{i}',
-                            reg_date=f'2002-05-12', )
+                            address=f'some address house.{i}',
+                            reg_date=generate_random_date(2022, 2023), )
             client.save()
             self.stdout.write(f'{client}')
-
--- GB_Django/hw2_app/models.py
+++ GB_Django/hw2_app/models.py
@@ -21,7 +21,7 @@ class Goods(models.Model):
     product_description = models.TextField()
     product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_quantity = models.IntegerField()
-    date_item_add = models.DateTimeField()
+    date_item_add = models.DateField()
 
     def __str__(self):
         return f'product_name: {self.product_name}, ' \
